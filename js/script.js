a6e27840264bd2fcb570a2b6f9d5f03e5b07e2d3
GLCVLL <g.ioele96@outlook.it>
2023-06-12 16:50:07 +0200

    added loop

--- js/script.js
+++ js/script.js
@@ -1,1 +1,54 @@
 console.log('JS OK');
+
+
+// prendo gli elementi dal dom
+
+const team = document.querySelectorAll('card');
+
+// creo l'array di oggetti con tutte le info
+
+teammates = [
+    {
+        fullName: 'Wayne Barnet',
+        position: 'Founder & CEO',
+    },
+    {
+        fullName: 'Angela Carolt',
+        position: 'Chief Editor',   
+    },
+    {
+        fullName: 'Walter Gordon',
+        position: 'Office Manager',
+    },
+    {
+        fullName: 'Angela Lopez',
+        position: 'Social Media Manager',   
+    },
+    {
+        fullName: 'Scott Estrada',
+        position: 'Developer',
+    },
+    {
+        fullName: 'Barbara Ramos',
+        position: 'Graphic Designer',   
+    },
+]
+
+// mi preparo la constante
+
+let teamCard = '';
+
+// creo il ciclo
+for (let i = 0; i < teammates.length; i++){
+    const teammate = teammates[i];
+    teamCard += `
+    <div class="card-body">
+    <div class="card-text text-center">
+        <h4>fullname: ${teammate.fullName}</h4>
+        <p>position: ${teammate.position}</p>
+    </div>`;
+}
+
+// stampo in pagina
+
+team.innerHTML = teamCard;
