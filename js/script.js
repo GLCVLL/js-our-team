98523ed9ad13d2538f888fc8d6519ff100362b98
GLCVLL <g.ioele96@outlook.it>
2023-06-12 17:10:41 +0200

    fixed error in loop

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ console.log('JS OK');
 
 // prendo gli elementi dal dom
 
-const team = document.querySelectorAll('card');
+const team = document.querySelector('col col-4 py-3');
 
 // creo l'array di oggetti con tutte le info
 
@@ -42,10 +42,13 @@ let teamCard = '';
 for (let i = 0; i < teammates.length; i++){
     const teammate = teammates[i];
     teamCard += `
-    <div class="card-body">
-    <div class="card-text text-center">
-        <h4>fullname: ${teammate.fullName}</h4>
-        <p>position: ${teammate.position}</p>
+    <div class="card">
+        <div class="card-body">
+            <div class="card-text text-center">
+                <h4>fullname: ${teammate.fullName}</h4>
+                <p>position: ${teammate.position}</p>
+            </div>
+        </div>
     </div>`;
 }
 
